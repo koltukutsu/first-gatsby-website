2b3f465302aa2db268b91a84962cd3caf9748bcd
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-31 20:34:43 +0300

    first steps to understand what is gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import * as React from 'react'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
-}
-// Step 3: Export your component
-export default IndexPage
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+    </Layout>
+  );
+};
+
+export default IndexPage;
