12e4a3e8532a5ad77d8082a47a598b1ea996e596
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-31 21:27:27 +0300

    learnt how to use graphql

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { heading } from "../components/layout.module.css";
 
 const AboutPage = () => {
   return (
     <main>
       <title>About Me</title>
-      <h1>About Me</h1>
+      <h1 className={heading}>About Me</h1>
       <Link to="/">Back to Home</Link>
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
